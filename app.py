11a39d849b97d7c29c9ff05afe6dcb2d833e625b
amit-y11 <69119846+amit-y11@users.noreply.github.com>
2021-02-13 10:57:12 +0530

    Add files via upload
    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from flask_cors import CORS
+from scrapeweb import scrape
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def welcome():
+    return 'Welcome to telegram live subcribers count api'
+
+@app.route('/getsubs/<channel>')
+def getsubs(channel):
+    result=scrape(channel)
+    return result
+
+@app.route('/getsubs')
+def subs_page():
+    return "Enter name of the channel in the format /getsubs/channel-name"
+
+
+if __name__=='__main__':
+    app.run()
---
+++ scrapeweb.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+def scrape(channel):
+    try:
+        page=requests.get("https://telegram.dog/"+channel)
+        soup=bs(page.text,'html.parser')
+        subs=soup.find("div",{"class":'tgme_page_extra'}).text.replace(" ","").split("members")[0]
+        channel_name=soup.find("div",{"class":'tgme_page_title'}).text.replace("\n","")
+        description=soup.find("div",{"class":'tgme_page_description'}).text
+        img=soup.find("img",{"class":'tgme_page_photo_image'})['src']
+        data = {}
+        data['subs']=subs
+        data['channel_name']=channel_name
+        data['description']=description
+        data['image']=img
+        return data
+    except Exception as e:
+        return {"Error":e}
+
+
